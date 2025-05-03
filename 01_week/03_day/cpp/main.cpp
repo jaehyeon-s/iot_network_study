fde5c927a2cc46e222ccfcca8d1b36cf37ed8e77
Jae <83636246+jaehyeon-s@users.noreply.github.com>
2025-05-04 02:09:10 +0900

    Add files via upload

---
+++ 01_week/03_day/cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// °è»ê±â
+
+	int a = 1 + 5;
+
+	double b = 1.5 - 40.2;
+
+	int c = 6 * 4;
+
+	double d = 35.0 / 24.0;
+	double e = 73 / 14;
+
+	int f = 14 % 4;
+
+	cout << a << " " << b << " " << c << " " << d << " " << e << " " << f;
+
+	return 0;
+}
---
+++ 01_week/03_day/rust/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let a = 1 + 5;
+
+    let b = 1.5 - 40.2;
+
+    let c = 6 * 4;
+
+    let d = 35.0 / 24.0;
+    let e = 73 / 14;
+
+    let f = 14 % 4;
+
+    println!("{a}, {b}, {c}, {d}, {e}, {f}");
+}
